0464a8dd6fe264dfad47dcad0e250367fe58c802
Xenodius <xenodius@hotmail.com>
2024-01-08 15:49:49 -0400

    added starter files and routing

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+// import Link to hyperlink the distinct pages in the navigation
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        // link for main path '/'
+        <li>
+          <Link to='/'>
+            <p>About Me</p>
+          </Link> 
+        </li>
+        // link for portfolio path 
+        <li>
+          <Link to='/portfolio'>
+            <p>Portfolio</p>
+          </Link> 
+        </li>
+        // link for contact path
+        <li>
+          <Link to='/contact'>
+            <p>Contact</p>
+          </Link> 
+        </li>
+        // link for resume path
+        <li>
+          <Link to='/resume'>
+            <p>Resume</p>
+          </Link> 
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
