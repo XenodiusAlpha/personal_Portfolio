d888c7c758530a19eb8cc25486aa968e35671997
Xenodius <xenodius@hotmail.com>
2024-01-09 10:52:11 -0400

    added form data to contact page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from '../assets/images/profile_pic.jpg'
 
 const AboutMe = () => {
   return (
-    <h1>About me page</h1>
+    <section>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          Hi! My name is <strong>Justin</strong>, and I'm a budding full-stack web developper. I've been in the automotive industry
+          for the past 5 years, where I've worked hard to get my red-seal certification. Now I've got my sights set on a new venture
+          in life, one that throught twist and turns of life had been pushed aside, that's right: <em>web developpment</em>.
+        </p>
+      </div>
+      <div>
+        <img src={Image} alt="" />
+      </div>
+    </section>
+    
   )
 };
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { useState } from 'react';
+
+
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submited');
+  }
+
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Contact</h1>
+      <form action={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" 
+               id='name'
+               name='name'
+               value={formData.name}
+               placeholder='Your name here'
+        />
+        <label htmlFor="email">Email:</label>
+        <input type="text" 
+               id='email'
+               name='email'
+               value={formData.email}
+               placeholder='Your email here'
+        />
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" 
+                  id="message" 
+                  cols="30" 
+                  rows="10" 
+                  placeholder='Your message here'
+                  value={formData.message}
+        />
+        <button type='Submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
