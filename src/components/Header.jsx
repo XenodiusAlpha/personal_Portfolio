b18e196f5203e349daffef306ef4fbea3d14f7a0
Xenodius <xenodius@hotmail.com>
2024-01-09 14:48:34 -0400

    added logos and functionality to footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,6 @@ const Header = () => {
         <h1>Justin Surette</h1>
         <Navigation />
     </header>
-    
   )
 }
 
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
         <p>
           Hi! My name is <strong>Justin</strong>, and I'm a budding full-stack web developper. I've been in the automotive industry
           for the past 5 years, where I've worked hard to get my red-seal certification. Now I've got my sights set on a new venture
-          in life, one that throught twist and turns of life had been pushed aside, that's right: <em>web developpment</em>.
+          in life, one that throught twist and turns of life had been pushed aside, that's right: <em>web developpment</em>!
         </p>
       </div>
       <div>
