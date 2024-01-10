f7b8925a0d930f75238a64019667e61a90121179
Xenodius <xenodius@hotmail.com>
2024-01-09 22:43:06 -0400

    added active link with css

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,31 +2,37 @@ import React from 'react'
 // import Link to hyperlink the distinct pages in the navigation
 import { Link } from 'react-router-dom'
 import './Navigation.css'
+import { useLocation } from 'react-router-dom';
 
 const Navigation = () => {
+  // variables for active location
+  const location = useLocation();
+  const {pathname} = location;
+  const splitLocation = pathname.split('/');
+
   return (
     <nav>
       <ul className='navList'>
         {/* link for main path '/' */}
-        <li>
+        <li className={splitLocation[1] === '' ? 'active' : ''}>
           <Link to='/'>
             <p>About Me</p>
           </Link> 
         </li>
         {/* link for portfolio path  */}
-        <li>
+        <li className={splitLocation[1] === 'portfolio' ? 'active' : ''}>
           <Link to='/portfolio'>
             <p>Portfolio</p>
           </Link> 
         </li>
         {/* link for contact path */}
-        <li>
+        <li className={splitLocation[1] === 'contact' ? 'active' : ''}>
           <Link to='/contact'>
             <p>Contact</p>
           </Link> 
         </li>
         {/* link for resume path */}
-        <li>
+        <li className={splitLocation[1] === 'resume' ? 'active' : ''}>
           <Link to='/resume'>
             <p>Resume</p>
           </Link> 
